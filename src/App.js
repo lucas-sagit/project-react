02ea34a6bf5534e1e0fe8ab5a5eca162f0253f56
lucas-sagit <lv011297@gmail.com>
2023-10-25 17:30:35 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Meu JSX, alterando algumas coisas!</h1>
+    
+    </div>
+  );
+}
+
+export default App;
